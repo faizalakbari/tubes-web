356e0ae25f61e72e9e327b755ce578dac5fb1232
faizalakbari <faizalakbariputra@gmail.com>
2019-04-18 09:24:07 +0700

    update model

---
+++ application/models/Kontak_model.php
@@ -0,0 +1,42 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kontak_model extends CI_Model
+{
+    private $_table = "kontak";
+
+    public $nama;
+    public $email;
+    public $telp;
+    public $pesan;
+
+    public function rules()
+    {
+        return [
+            ['field' => 'nama',
+            'label' => 'Nama',
+            'rules' => 'required'],
+
+            ['field' => 'email',
+            'label' => 'Email',
+            'rules' => 'required'],
+
+            ['field' => 'telp',
+            'label' => 'Phone Number',
+            'rules' => 'numeric'],
+            
+            ['field' => 'pesan',
+            'label' => 'Message',
+            'rules' => 'required']
+        ];
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        $this->nama = $post["nama"];
+        $this->email = $post["email"];
+        $this->telp = $post["telp"];
+        $this->pesan = $post["pesan"];
+        $this->db->insert($this->_table, $this);
+    }
+}
